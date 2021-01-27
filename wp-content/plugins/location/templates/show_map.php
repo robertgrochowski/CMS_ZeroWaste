953ad687070484c1023634f4cd113be6b333874b
Marek Parr <marek.parr@gmail.com>
2021-01-27 19:13:08 +0100

    fixed show_map template

--- wp-content/plugins/location/templates/show_map.php
+++ wp-content/plugins/location/templates/show_map.php
@@ -1,9 +1,5 @@
 <div>
     <div id="map"></div>
-    <script defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgFb65StJaH_YvREznt-NzWfM1JicH0k8&callback=initMap">
-    </script>
-
     <style>
         #map {
             height: 300px;
@@ -13,26 +9,40 @@
 
     <?php
 
+    global $product;
+
     $latitude = get_post_meta($product->get_id(), 'latitude', true);
     $longitude = get_post_meta($product->get_id(), 'longitude', true);
 
     ?>
 
-    <script>
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: new google.maps.LatLng(<?php echo $latitude?>,
-                    <?php echo $longitude?>),
-                zoom: 15
-            });
 
-            var point = new google.maps.LatLng(<?php echo $latitude?>,
-                <?php echo $longitude?>);
+    <?php if ($latitude != null && $longitude != null): ?>
 
-            var marker = new google.maps.Marker({
-                map: map,
-                position: point
-            });
-        }
-    </script>
+        <script defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgFb65StJaH_YvREznt-NzWfM1JicH0k8&callback=initMap">
+        </script>
+
+    <?php endif ?>
+
+    <?php if (get_post_meta($product->get_id(), 'latitude', true) != null
+    && get_post_meta($product->get_id(), 'longitude', true) != null): ?>
+        <script>
+            function initMap() {
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    center: new google.maps.LatLng(<?php echo $latitude?>,
+                        <?php echo $longitude?>),
+                    zoom: 15
+                });
+
+                var point = new google.maps.LatLng(<?php echo $latitude?>,
+                    <?php echo $longitude?>);
+
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: point
+                });
+            }
+        </script>
+    <?php endif ?>
 </div>
